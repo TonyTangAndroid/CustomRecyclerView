82b710c500a4ddff83c0618058b87862255c3499
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-06-27 14:25:58 -0700

    Migrate to Google Java Format

--- app/src/main/java/com/hariofspades/customrecyclerview/MainActivity.java
+++ app/src/main/java/com/hariofspades/customrecyclerview/MainActivity.java
@@ -2,196 +2,201 @@ package com.hariofspades.customrecyclerview;
 
 import android.content.Context;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.appcompat.widget.Toolbar;
 import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.hariofspades.customrecyclerview.Adapter.CustomAdapter;
 import com.hariofspades.customrecyclerview.Pojo.CustomPojo;
-
 import java.util.ArrayList;
 
-/** Authuor: Hari vignesh Jayapalan
- *  Created on: 6 Feb 2016
- *  Email: hariutd@gmail.com
+/**
+ * Authuor: Hari vignesh Jayapalan Created on: 6 Feb 2016 Email: hariutd@gmail.com
  *
- *  Implementing custom RecyclerView Adapter
- *  Tutorial @ https://medium.com/@harivigneshjayapalan
- * */
+ * <p>Implementing custom RecyclerView Adapter Tutorial @ https://medium.com/@harivigneshjayapalan
+ */
 public class MainActivity extends AppCompatActivity {
-    //Declare the Adapter, AecyclerView and our custom ArrayList
-    RecyclerView recyclerView;
-    CustomAdapter adapter;
-    private ArrayList<CustomPojo> listContentArr= new ArrayList<>();
+  // Declare the Adapter, AecyclerView and our custom ArrayList
+  RecyclerView recyclerView;
+  CustomAdapter adapter;
+  private ArrayList<CustomPojo> listContentArr = new ArrayList<>();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    fab.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show();
+          }
         });
 
-        recyclerView=(RecyclerView)findViewById(R.id.recycleView);
-        //As explained in the tutorial, LineatLayoutManager tells the RecyclerView that the view
-        //must be arranged in linear fashion
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        /**
-         * RecyclerView: Implementing single item click and long press (Part-II)
-         * */
-        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(this,
-                recyclerView, new ClickListener() {
-            @Override
-            public void onClick(View view, final int position) {
-                //Values are passing to activity & to fragment as well
-                Toast.makeText(MainActivity.this, "Single Click on position :"+position,
-                        Toast.LENGTH_SHORT).show();
-                ImageView picture=(ImageView)view.findViewById(R.id.picture);
-                picture.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(MainActivity.this, "Single Click on Image :"+position,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
+    recyclerView = (RecyclerView) findViewById(R.id.recycleView);
+    // As explained in the tutorial, LineatLayoutManager tells the RecyclerView that the view
+    // must be arranged in linear fashion
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    /** RecyclerView: Implementing single item click and long press (Part-II) */
+    recyclerView.addOnItemTouchListener(
+        new RecyclerTouchListener(
+            this,
+            recyclerView,
+            new ClickListener() {
+              @Override
+              public void onClick(View view, final int position) {
+                // Values are passing to activity & to fragment as well
+                Toast.makeText(
+                        MainActivity.this,
+                        "Single Click on position :" + position,
+                        Toast.LENGTH_SHORT)
+                    .show();
+                ImageView picture = (ImageView) view.findViewById(R.id.picture);
+                picture.setOnClickListener(
+                    new View.OnClickListener() {
+                      @Override
+                      public void onClick(View v) {
+                        Toast.makeText(
+                                MainActivity.this,
+                                "Single Click on Image :" + position,
+                                Toast.LENGTH_SHORT)
+                            .show();
+                      }
+                    });
+              }
 
-            @Override
-            public void onLongClick(View view, int position) {
-                Toast.makeText(MainActivity.this, "Long press on position :"+position,
-                        Toast.LENGTH_LONG).show();
-            }
-        }));
+              @Override
+              public void onLongClick(View view, int position) {
+                Toast.makeText(
+                        MainActivity.this, "Long press on position :" + position, Toast.LENGTH_LONG)
+                    .show();
+              }
+            }));
 
-        adapter=new CustomAdapter(this);
-        //Method call for populating the view
-        populateRecyclerViewValues();
-    }
+    adapter = new CustomAdapter(this);
+    // Method call for populating the view
+    populateRecyclerViewValues();
+  }
 
-    private void populateRecyclerViewValues() {
-        /** This is where we pass the data to the adpater using POJO class.
-         *  The for loop here is optional. I've just populated same data for 50 times.
-         *  You can use a JSON object request to gather the required values and populate in the
-         *  RecyclerView.
-         * */
-        for(int iter=0;iter<=50;iter++) {
-            //Creating POJO class object
-            CustomPojo pojoObject = new CustomPojo();
-            //Values are binded using set method of the POJO class
-            pojoObject.setName("Hari Vigensh Jayapalan");
-            pojoObject.setContent("Hello RecyclerView! item: "+iter);
-            pojoObject.setTime("10:45PM");
-            //After setting the values, we add all the Objects to the array
-            //Hence, listConentArr is a collection of Array of POJO objects
-            listContentArr.add(pojoObject);
-        }
-        //We set the array to the adapter
-        adapter.setListContent(listContentArr);
-        //We in turn set the adapter to the RecyclerView
-        recyclerView.setAdapter(adapter);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
+  private void populateRecyclerViewValues() {
     /**
-     * RecyclerView: Implementing single item click and long press (Part-II)
-     *
-     * - creating an Interface for single tap and long press
-     * - Parameters are its respective view and its position
-     * */
-
-    public static interface ClickListener{
-        public void onClick(View view,int position);
-        public void onLongClick(View view,int position);
+     * This is where we pass the data to the adpater using POJO class. The for loop here is
+     * optional. I've just populated same data for 50 times. You can use a JSON object request to
+     * gather the required values and populate in the RecyclerView.
+     */
+    for (int iter = 0; iter <= 50; iter++) {
+      // Creating POJO class object
+      CustomPojo pojoObject = new CustomPojo();
+      // Values are binded using set method of the POJO class
+      pojoObject.setName("Hari Vigensh Jayapalan");
+      pojoObject.setContent("Hello RecyclerView! item: " + iter);
+      pojoObject.setTime("10:45PM");
+      // After setting the values, we add all the Objects to the array
+      // Hence, listConentArr is a collection of Array of POJO objects
+      listContentArr.add(pojoObject);
     }
+    // We set the array to the adapter
+    adapter.setListContent(listContentArr);
+    // We in turn set the adapter to the RecyclerView
+    recyclerView.setAdapter(adapter);
+  }
 
-    /**
-     * RecyclerView: Implementing single item click and long press (Part-II)
-     *
-     * - creating an innerclass implementing RevyvlerView.OnItemTouchListener
-     * - Pass clickListener interface as parameter
-     * */
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
 
-    class RecyclerTouchListener implements RecyclerView.OnItemTouchListener{
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
 
-        private ClickListener clicklistener;
-        private GestureDetector gestureDetector;
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
 
-        public RecyclerTouchListener(Context context, final RecyclerView recycleView, final ClickListener clicklistener){
+  /**
+   * RecyclerView: Implementing single item click and long press (Part-II)
+   *
+   * <p>- creating an Interface for single tap and long press - Parameters are its respective view
+   * and its position
+   */
+  public static interface ClickListener {
+    public void onClick(View view, int position);
 
-            this.clicklistener=clicklistener;
-            gestureDetector=new GestureDetector(context,new GestureDetector.SimpleOnGestureListener(){
+    public void onLongClick(View view, int position);
+  }
+
+  /**
+   * RecyclerView: Implementing single item click and long press (Part-II)
+   *
+   * <p>- creating an innerclass implementing RevyvlerView.OnItemTouchListener - Pass clickListener
+   * interface as parameter
+   */
+  class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
+
+    private ClickListener clicklistener;
+    private GestureDetector gestureDetector;
+
+    public RecyclerTouchListener(
+        Context context, final RecyclerView recycleView, final ClickListener clicklistener) {
+
+      this.clicklistener = clicklistener;
+      gestureDetector =
+          new GestureDetector(
+              context,
+              new GestureDetector.SimpleOnGestureListener() {
                 @Override
                 public boolean onSingleTapUp(MotionEvent e) {
-                    return true;
+                  return true;
                 }
 
                 @Override
                 public void onLongPress(MotionEvent e) {
-                    View child=recycleView.findChildViewUnder(e.getX(),e.getY());
-                    if(child!=null && clicklistener!=null){
-                        clicklistener.onLongClick(child,recycleView.getChildAdapterPosition(child));
-                    }
+                  View child = recycleView.findChildViewUnder(e.getX(), e.getY());
+                  if (child != null && clicklistener != null) {
+                    clicklistener.onLongClick(child, recycleView.getChildAdapterPosition(child));
+                  }
                 }
-            });
-        }
-
-        @Override
-        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-            View child=rv.findChildViewUnder(e.getX(),e.getY());
-            if(child!=null && clicklistener!=null && gestureDetector.onTouchEvent(e)){
-                clicklistener.onClick(child,rv.getChildAdapterPosition(child));
-            }
-
-            return false;
-        }
-
-        @Override
-        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-
-        }
-
-        @Override
-        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-
-        }
+              });
     }
 
+    @Override
+    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+      View child = rv.findChildViewUnder(e.getX(), e.getY());
+      if (child != null && clicklistener != null && gestureDetector.onTouchEvent(e)) {
+        clicklistener.onClick(child, rv.getChildAdapterPosition(child));
+      }
+
+      return false;
+    }
+
+    @Override
+    public void onTouchEvent(RecyclerView rv, MotionEvent e) {}
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {}
+  }
 }
--- app/src/main/java/com/hariofspades/customrecyclerview/Pojo/CustomPojo.java
+++ app/src/main/java/com/hariofspades/customrecyclerview/Pojo/CustomPojo.java
@@ -2,39 +2,42 @@ package com.hariofspades.customrecyclerview.Pojo;
 
 import java.util.ArrayList;
 
-/** Authuor: Hari vignesh Jayapalan
- *  Created on: 6 Feb 2016
- *  Email: hariutd@gmail.com
+/**
+ * Authuor: Hari vignesh Jayapalan Created on: 6 Feb 2016 Email: hariutd@gmail.com
  *
- *  Implementing custom RecyclerView Adapter
- *  Tutorial @ https://medium.com/@harivigneshjayapalan
- * */
+ * <p>Implementing custom RecyclerView Adapter Tutorial @ https://medium.com/@harivigneshjayapalan
+ */
 public class CustomPojo {
 
-    //POJO class consists of get method and set method
-    private String name;
-    private String time,content;
-    private ArrayList<CustomPojo> customPojo =new ArrayList<>();
+  // POJO class consists of get method and set method
+  private String name;
+  private String time, content;
+  private ArrayList<CustomPojo> customPojo = new ArrayList<>();
 
+  public CustomPojo() {}
 
+  // getting content value
+  public String getContent() {
+    return content;
+  }
+  // setting content value
+  public void setContent(String content) {
+    this.content = content;
+  }
 
-    public CustomPojo() {
+  public String getTime() {
+    return time;
+  }
 
-    }
-    //getting content value
-    public String getContent(){return content;}
-    //setting content value
-    public void setContent(String content){this.content=content;}
+  public void setTime(String time) {
+    this.time = time;
+  }
 
-    public String getTime(){return time;}
-    public void setTime(String time){this.time=time;}
+  public String getName() {
+    return name;
+  }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+  public void setName(String name) {
+    this.name = name;
+  }
 }
