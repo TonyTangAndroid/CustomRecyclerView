eef4f9f9c0603a6308fb7ec507f7edfc4b8eaa15
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-06-27 14:20:51 -0700

    Migrate to AndroidX

--- app/src/main/java/com/hariofspades/customrecyclerview/MainActivity.java
+++ app/src/main/java/com/hariofspades/customrecyclerview/MainActivity.java
@@ -2,12 +2,12 @@ package com.hariofspades.customrecyclerview;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.Toolbar;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
